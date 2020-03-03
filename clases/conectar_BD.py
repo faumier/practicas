cb92638ed852189e35adcfa66e84692bba541ee8
unknown <faumiercangrejo@gmail.com>
2020-03-03 14:32:58 -0500

    "primer commit"

---
+++ clases/conectar_BD.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+class conect ():
+
+    def __init__ (self):
+        self.conector = sqlite3.connect("ejercicio")
+        self.cursor = self.conector.cursor()
+
+    def peticion(self, sql):
+
+        self.cursor.execute(sql)
+
+        self.conector.commit()
+
+        self.conector.close()
+    
+
+        
+
